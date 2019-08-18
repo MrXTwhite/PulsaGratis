f64d09ba3f91357d3b7d07d7a7a0a8c6e6f455a3
MrXTwhite <54221262+MrXTwhite@users.noreply.github.com>
2019-08-18 19:17:46 +0700

    PULSA.sh
    Gabut

---
+++ PULSA.sh
@@ -0,0 +1,113 @@
+#!/system/xbin/bash
+#Bersihkan Layar
+clear
+blue='\e[1;34m'
+green='\e[1;32m'
+purple='\[1;35m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+white='\e[1;37m'
+yellow='\e[1;33m'
+sleep 2s;
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+toilet -f standard -F gay "PULSAGRATIS"
+echo "\033[31;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo "LAGI GABUT GUE BREE"
+echo "Tools Gabut By MR.xtWHITE"
+echo "Author : " MR.xtWHITE"
+" Team : " Cyber Attack Army"
+echo " WhatsApp : " +1 315 493 8588"
+" Pesan : "Jangan Di Salah Gunakan Dan Disebar :) Karena Nanti Dosa Tanggung Sendiri:)"
+sleep 2s;
+echo "ThanksTo:"
+echo "Allmember Cyber Attack Army"
+echo "\033[32;1m=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*=*"
+echo ""
+sleep 3s;
+echo "Kumpulan Kumpulan Pulsa All Operator"
+echo "Gunakan Pakai +62 #Contoh +62xxx"
+echo "Masukin Nomer Lu Bree :"
+read p;
+
+echo "Nomor Sedang Di Proses"
+sleep 5s;
+echo "\033[37;1m█10%" 
+sleep 10s;
+echo "██20%" 
+sleep 20s;
+echo "███30%" 
+sleep 20s;
+echo "████40%" 
+sleep 20s;
+echo "█████50%" 
+sleep 20s;
+echo "██████60%" 
+sleep 20s;
+echo "████████80%"
+sleep 30s;
+echo "\033[31;1mNomor Gagal Di Proses..""\033[32;1mKesalahan Jaringan Masukan Lagi Nomor Anda:"
+read p;
+sleep 2s;
+echo "Sedang Menuju Proses..."
+sleep 2s;
+echo "\033[37;1m█10%"
+sleep 10s;
+echo "██20%"
+sleep 10s;
+echo "███30%"
+sleep 10s;
+echo "████40%"
+sleep 10s;
+echo "█████50%"
+sleep 10s;
+echo "██████60%"
+sleep 10s;
+echo "███████70%"
+sleep 10s;
+echo "████████80%"
+sleep 10s;
+echo "█████████90%"
+sleep 10s;
+echo "██████████100%"
+sleep 10s;
+echo "\033[32;1mSilahkan Masukan Jumlah Nya Minimal 5000 :"
+read p;
+sleep 2s;
+echo "Sedang Menuju Proses..."
+echo "\033[37;1m█10%"
+sleep 10s;
+echo "██20%"
+sleep 10s;
+echo "███30%"
+sleep 10s;
+echo "████40%"
+sleep 10s;
+echo "█████50%"
+sleep 10s;
+echo "██████60%"
+sleep 10s;
+echo "███████70%"
+sleep 10s;
+echo "████████80%"
+sleep 10s;
+echo "█████████90%"
+sleep 10s;
+echo "██████████100%"
+sleep 20s;
+echo "\033[34;1mJumlah Yang Anda Masukkan Sukses...""\033[32;1mSilahkan Tunggu 1 Menit..."
+echo "\033[32;1mJangan Di Close Dulu.."
+sleep 20s;
+sleep 8s;
+echo "Sabar Ya Bree"
+sleep 10s;
+sleep 8s;
+sleep 8s;
+
+echo "Anda Telah Di Tipu Sama Abang Konter Nya Awokawokawok"
+echo "Awokawok Nama Nya Juga Tools GABUT Bree Awokawok"
+sleep 2s;
+echo "Jangan Baper Yaa Bree Awokawok"
+sleep 1s;
+echo "Yang Sabar Yaa Bree Gue Minta Maaf Awokawokawok"
+sleep 1s;
+exit
